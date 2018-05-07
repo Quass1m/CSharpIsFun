ae4c792bae7c44510ce59f64555f3d42b886604e
Konrad Pietralik <konrad.pietralik@gmail.com>
2018-05-07 20:42:06 +0200

    EqualityTests

---
+++ CSharpIsFunTests/EqualityTests.cs
@@ -0,0 +1,43 @@
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace CSharpIsFunTests
+{
+    public class EqualityTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            object o1 = null;
+            object o2 = new object();
+
+            //Technically, these should read object.ReferenceEquals for clarity, but this is redundant.
+            ReferenceEquals(o1, o1).Should().BeTrue();
+            ReferenceEquals(o1, o2).Should().BeFalse();
+            ReferenceEquals(o2, o1).Should().BeFalse();
+            ReferenceEquals(o2, o2).Should().BeTrue();
+
+            Action res1 = (() => { o1.Equals(o1); });
+            res1.Should().Throw<NullReferenceException>();
+            Action res2 = (() => { o1.Equals(o2); });
+            res2.Should().Throw<NullReferenceException>();
+            o2.Equals(o1).Should().BeFalse();
+            o2.Equals(o2).Should().BeTrue();
+
+            o2.Equals(new object()).Should().BeFalse();
+            (o1 == o2).Should().BeFalse();
+
+            object o1prim = o1;
+            object o2prim = o2;
+
+            ReferenceEquals(o1, o1prim).Should().BeTrue();
+            ReferenceEquals(o1prim, o2prim).Should().BeFalse();
+            ReferenceEquals(o2prim, o1prim).Should().BeFalse();
+            ReferenceEquals(o2, o2prim).Should().BeTrue();
+
+            (o1 == o1prim).Should().BeTrue();
+            (o2 == o2prim).Should().BeTrue();
+        }
+    }
+}
