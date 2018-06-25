7563affe3cc364c80978b8e5ccfa231d9b3525e2
Konrad Pietralik <konrad.pietralik@gmail.com>
2018-06-25 23:07:42 +0200

    Function Composition

---
+++ CSharpIsFun/Features/FunctionComposition.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+using static CSharpIsFun.Helpers.FPHelper;
+
+namespace CSharpIsFun.Features
+{
+    /// <summary>
+    /// Example of C# Functional Programming
+    /// Function Composition
+    /// </summary>
+    /// <see cref="https://devstyle.pl/2018/06/14/funkcyjna-kompozycja-w-c-sharp/"/>
+    public class FunctionComposition
+    {
+        public static void ExampleIExtradingComposition()
+        {
+            Func<string, string> normalizeSymbol = symbol => symbol.ToLower();
+
+            var fetchChart = Curry<string, string, string>((period, symbol) =>
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    return httpClient.GetStringAsync($"https://api.iextrading.com/1.0/stock/{symbol}/chart/{period}").Result;
+                }
+            });
+
+            Func<string, ChartItem> getChartItems = Compose(
+                normalizeSymbol,
+                fetchChart("1m"),
+                JsonConvert.DeserializeObject<ChartItem[]>,
+                Reduce<ChartItem>((candidate, item) => candidate.Close > item.Close ? candidate : item)
+            );
+
+            var max = getChartItems("AAPL");
+            Console.WriteLine(max.Close);
+        }
+
+        private class ChartItem
+        {
+            public DateTime Date { get; set; }
+            public double Close { get; set; }
+        }
+    }
+}
---
+++ CSharpIsFun/Helpers/FPHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharpIsFun.Helpers
+{
+    public static class FPHelper
+    {
+        public static Func<T1, Func<T2, R>> Curry<T1, T2, R>(Func<T1, T2, R> func)
+        {
+            return x => y => func(x, y);
+        }
+
+        public static Func<T1, Func<T2, Func<T3, R>>> Curry<T1, T2, T3, R>(Func<T1, T2, T3, R> func)
+        {
+            return x => y => z => func(x, y, z);
+        }
+
+        public static Func<X, Z> Compose<X, Y, Z>(Func<X, Y> f, Func<Y, Z> g)
+        {
+            return x => g(f(x));
+        }
+
+        public static Func<T1, T4> Compose<T1, T2, T3, T4>(Func<T1, T2> f1, Func<T2, T3> f2, Func<T3, T4> f3)
+        {
+            return x => f3(f2(f1(x)));
+        }
+
+        public static Func<T1, T5> Compose<T1, T2, T3, T4, T5>(Func<T1, T2> f1, Func<T2, T3> f2, Func<T3, T4> f3, Func<T4, T5> f4)
+        {
+            return x => f4(f3(f2(f1(x))));
+        }
+
+        public static T Reduce<T>(Func<T, T, T> reducer, IEnumerable<T> items)
+        {
+            return items.Aggregate(reducer);
+        }
+
+        public static Func<IEnumerable<T>, T> Reduce<T>(Func<T, T, T> reducer)
+        {
+            return items => items.Aggregate(reducer);
+        }
+    }
+}
